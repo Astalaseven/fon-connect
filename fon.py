3159344cd9c8638e85b50ff1ef4cf305276ed163
Astalaseven <gatien.bovyn@gmail.com>
2013-08-06 14:23:54 +0200

    Initial commit

---
+++ fon.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python2
+
+from mechanize import ParseResponse, urlopen, URLError
+
+uri = "http://perdu.com"
+user_name = "user@belgacomfon.be"
+user_pass = "8_character_password"
+
+def internet_on():
+    try:
+        response=urlopen(uri, timeout=1)
+        return True
+    except URLError as err: pass
+    return False
+
+connection_active = internet_on()
+try:
+    response = urlopen(uri)
+    forms = ParseResponse(response, backwards_compat=False)
+    form = forms[0]
+    print('Sending login...')
+    form["login[user]"] = user_name
+    form["login[pass]"] = user_pass
+except IndexError:
+    if connection_active:
+        print('You\'re already connected')
+    else:
+        print('No form found / Are you sure you are connected on a FON box?')
+except URLError:
+    print('Not connected to a FON box')
+
+# if not internet_on():
+try:
+    urlopen(form.click()).read()
+    print('Connected to FON Network!')
+except NameError:
+    if not connection_active:
+        print('Not connected to FON Network')
+
